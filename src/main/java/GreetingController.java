be475dcbd5dec33b1fa1326d51fada607dfa0663
Niko Köbler <niko@n-k.de>
2016-05-14 15:19:00 +0200

    add keycloak resources and access security objects via standard security context

--- src/main/java/GreetingController.java
+++ src/main/java/GreetingController.java
@@ -1,5 +1,10 @@
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.representations.AccessToken;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
 
 /**
  * @author Niko Köbler, http://www.n-k.de, @dasniko
@@ -7,9 +12,15 @@ import javax.ws.rs.Path;
 @Path("greeter")
 public class GreetingController {
 
+    @Context
+    SecurityContext securityContext;
+
     @GET
     @Path("greet")
     public String getGreeting() {
-        return "Hello ";
+
+        AccessToken accessToken = ((KeycloakPrincipal) securityContext.getUserPrincipal()).getKeycloakSecurityContext().getToken();
+
+        return "Hello " + accessToken.getPreferredUsername();
     }
 }
